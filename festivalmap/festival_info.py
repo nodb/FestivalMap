857daf644ce1dce380d628e2bf60d54031665366
nodb <scnoh02@naver.com>
2023-11-18 23:27:38 +0900

    update : img 파싱 방식 변경

--- festivalmap/festival_info.py
+++ festivalmap/festival_info.py
@@ -9,48 +9,31 @@ def festival(id):
 
     response = requests.get(url=URL)
     soup = BeautifulSoup(response.text, "lxml")
-    '''
-    name : 이름
-    title : 주제
-    content : 내용
-    bg_img : 배경 이미지
-    img_num : 이미지 개수
-    img_JPG : 확장자 JPG인 이미지 번호
-    date : 날짜
-    location : 장소
-    price : 가격
-    partner : 주관사
-    tell : 전화번호
-    insta : 인스타그램 아이디
-    homepage : 홈페이지
-    '''
+    
+    # name : 이름
+    # title : 주제
+    # content : 내용
+    # img : 이미지
+    # date : 날짜
+    # location : 장소
+    # price : 가격
+    # partner : 주관사
+    # tell : 전화번호
+    # insta : 인스타그램 아이디
+    # homepage : 홈페이지
+    
     name = soup.find("h2", {"id": "festival_head"}).text
     title = soup.find("div", {"class": "slide_content"}).text
     title = title[:len(title)-4].strip()
     content = soup.find("p", {"class": "slide_content"}).text
     content = content[content.find("]")+1:].strip()
 
-    img_num = 1
-    img_jpg = []
-    img_JPG = []
-    for i in range(2, 20):
-        # img 상태코드로 존재여부 확인 및 확장자별 분리
-        try:
-            urllib.request.urlopen(
-                f'https://cdn.visitkorea.or.kr/kfes/upload/contents/db/{id}_{i}.png').status
-        except HTTPError:
-            try:
-                urllib.request.urlopen(
-                    f'https://cdn.visitkorea.or.kr/kfes/upload/contents/db/{id}_{i}.jpg').status
-                img_jpg.append(i)
-            except HTTPError:
-                try:
-                    urllib.request.urlopen(
-                        f'https://cdn.visitkorea.or.kr/kfes/upload/contents/db/{id}_{i}.JPG').status
-                    img_JPG.append(i)
-                except HTTPError:
-                    img_num = i - 1
-                    break
+    img = []
+    img.append(soup.find("meta", {"property": "og:image"})["content"])
+    bg = soup.find("div", {"class": "visula_bg"})["style"]
+    img.append(bg[bg.find("https"):bg.find(")")])
+    for i in soup.find_all("img", {"onerror": "this.src='/kfes/resources/img/default_detail_02.png';"}):
+        img.append(i["src"])
 
     insta = ''
     homepage = ''
@@ -76,9 +59,7 @@ def festival(id):
         'name': name,
         'title': title,
         'content': content,
-        'img_num': img_num,
-        'img_jpg': img_jpg,
-        'img_JPG': img_JPG,
+        'img': img,
         'date': date,
         'address': address,
         'price': price,
